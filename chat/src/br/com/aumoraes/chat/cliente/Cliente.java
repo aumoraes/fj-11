57960c6a23ea79df17d20df24048c5dd587b4117
aumoraes <aumoraes@sp.r7.com>
2017-06-13 16:48:42 -0300

    Incluindo projeto de chat

---
+++ chat/src/br/com/aumoraes/chat/cliente/Cliente.java
@@ -0,0 +1,48 @@
+package br.com.aurelio.chat.cliente;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+
+public class Cliente {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// dispara cliente
+		new Cliente("10.110.60.86", 12345).executa();		
+	}
+	
+	private String host;
+	private int porta;
+	
+	public Cliente (String host, int porta) {
+		this.host = host;
+		this.porta = porta;
+	}
+	
+	public void executa() throws UnknownHostException, IOException {
+		
+		Scanner inputName = new Scanner(System.in);
+		String userName = inputName.nextLine();
+		
+		Socket cliente = new Socket(this.host, this.porta);
+		
+		
+		System.out.println("O cliente se conectou ao servidor!");
+		
+		// thread para receber mensagens do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream());		
+		new Thread(r).start();
+		
+		// le msgs do teclado e manda pro servidor
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		while (teclado.hasNextLine()) {
+			saida.println(userName + " says: " + teclado.nextLine());
+		}
+		saida.close();
+		teclado.close();
+		cliente.close();
+	}
+}
---
+++ chat/src/br/com/aumoraes/chat/cliente/Recebedor.java
@@ -0,0 +1,23 @@
+package br.com.aurelio.chat.cliente;
+
+import java.io.InputStream;
+
+import java.util.Scanner;
+
+public class Recebedor implements Runnable{
+	
+	private InputStream servidor;
+	
+	public Recebedor(InputStream servidor) {
+		this.servidor = servidor;
+	}
+	
+	public void run() {
+		// recebe msgs do servidor e imprime na tela
+		Scanner s = new Scanner(this.servidor);
+		while (s.hasNextLine()) {
+			System.out.println(s.nextLine());
+		}
+	}
+	
+}
