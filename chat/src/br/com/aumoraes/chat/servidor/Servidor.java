a63a41de0b155d6e091f9c8c0087146b9d405e7c
aumoraes <aumoraes@sp.r7.com>
2017-06-21 13:02:40 -0300

    Atualizando controle de logout no lado do servidor

--- chat/src/br/com/aumoraes/chat/servidor/Servidor.java
+++ chat/src/br/com/aumoraes/chat/servidor/Servidor.java
@@ -15,7 +15,8 @@ public class Servidor {
 	
 	private int porta;
 	private List<Usuario> listaDeUsuarios; 
-	private int identificador = 0;
+	private int userId = 0;
+	private int connections = 0;
 	
 	public Servidor (int porta) {
 		this.porta = porta;
@@ -28,14 +29,16 @@ public class Servidor {
 		
 			System.out.println("O servidor está de pé na porta: " + this.porta);
 			
+			
+			
 			while (true) {
 				// aceita um cliente
 				Socket cliente = servidor.accept();
 				
 				Usuario user = new Usuario();
 				user.setIp( cliente.getInetAddress().getHostAddress() );
-				user.setId( identificador );
-				user.setNome( "cliente " + identificador );
+				user.setId( userId );
+				user.setNome( "cliente " + userId );
 				user.setColetorDeMensagem( cliente.getInputStream() );
 				System.out.println("Nova conexão com o cliente id: " +	user.getId() + ", ip: " + user.getIp() + ", nome: " + user.getNome() );
 				
@@ -50,16 +53,29 @@ public class Servidor {
 				// cria tratador de cliente numa nova thread
 				TrataCliente tc = new TrataCliente(this, user);
 				new Thread(tc).start();
-				identificador++;
+				userId++;
+				connections++;
+				mostraCliente();
 			}
 		} catch (Exception e) {
 			System.out.println("O Servidor está parado, motivo: " + e);
 		}
+		
+		
+		
 	}
 	public void remove( Usuario user ) {
 		distribuiMensagem("Info: O usuario " + user.getNome() + " se desconectou");
-		this.listaDeUsuarios.remove( user.getId() );
-		identificador--;
+		
+		
+		//TODO Por que esse metodo da pau?
+		try {
+			this.listaDeUsuarios.remove( user );
+		} catch (Exception e) {
+			System.out.println("aconteceu algum erro aqui>>>>> ");
+		}
+		
+		connections--;
 		System.out.println("cliente se desconectou " + user.toString());
 	}
 	
@@ -73,8 +89,15 @@ public class Servidor {
 	
 
 	public void mostraCliente(){
+		System.out.println("** Lista de usuarios ***");
 		for (Usuario user : this.listaDeUsuarios) {
 			System.out.println( user.toString() );
-		}		
+		}
+		
+		totalClientes();
+	}
+	
+	public void totalClientes(){
+		System.out.println("numero de pessoas conectadas: " + this.connections);
 	}
 }
